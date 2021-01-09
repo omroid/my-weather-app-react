4a3bb35efdb7b1fde35eb8a24e852136e3e79fb5
Oneg <omroid@gmail.com>
2021-01-09 14:49:00 +0200

    Updates

---
+++ precache-manifest.0c811c6f7f1956dd2b8b19684b72f574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3a4455ae7f2ed00e479a69de056880",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92487f80d485bf81535b",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "75d430ead4c36d85da42",
+    "url": "/static/css/main.4f43a46b.chunk.css"
+  },
+  {
+    "revision": "92487f80d485bf81535b",
+    "url": "/static/js/2.bf225e7f.chunk.js"
+  },
+  {
+    "revision": "75d430ead4c36d85da42",
+    "url": "/static/js/main.7278abab.chunk.js"
+  },
+  {
+    "revision": "dda56cb7c8283951c588",
+    "url": "/static/js/runtime~main.1f8107a1.js"
+  }
+]);
